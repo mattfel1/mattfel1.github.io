7b0cfe906bb8fa07172bb9ec7a17e1c2bf4535ba
Matthew <mattfel@stanford.edu>
2016-12-20 20:07:22 -0800

    auto update

--- bashrc/get.sh
+++ bashrc/get.sh
@@ -10,7 +10,7 @@ for s in ${servers[@]}; do
   fi
   scp -o ConnectTimeout=1 ${cmd}:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.txt
   if [[ $? = 0 ]]; then # success
-    st="sed -i 's/rgb(.*)\"><font color=\"rgb(.*)\">.*${s} bashrc/rgb(0,140,0)\"><font color=\"rgb(0,140,0)\">✓ ${s} bashrc/g' index.html"
+    st="sed -i 's/rgb(.*)\"><font color=\"rgb(.*)\">.*${s} bashrc/rgb(5,140,5)\"><font color=\"rgb(5,140,5)\">✓ ${s} bashrc/g' index.html"
     eval $st
   else # fail
     st="sed -i 's/rgb(.*)\"><font color=\"rgb(.*)\">.*${s} bashrc/rgb(255,0,0)\"><font color=\"rgb(255,0,0)\">× ${s} bashrc/g' index.html"
@@ -18,7 +18,7 @@ for s in ${servers[@]}; do
   fi
   scp -o ConnectTimeout=1 ${cmd}:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.txt
   if [[ $? = 0 ]]; then # success
-    st="sed -i 's/rgb(.*)\"><font color=\"rgb(.*)\">.*${s} bash_alias/rgb(0,140,0)\"><font color=\"rgb(0,140,0)\">✓ ${s} bash_alias/g' index.html"
+    st="sed -i 's/rgb(.*)\"><font color=\"rgb(.*)\">.*${s} bash_alias/rgb(5,140,5)\"><font color=\"rgb(5,140,5)\">✓ ${s} bash_alias/g' index.html"
     eval $st
   else # fail
     st="sed -i 's/rgb(.*)\"><font color=\"rgb(.*)\">.*${s} bash_alias/rgb(255,0,0)\"><font color=\"rgb(255,0,0)\">× ${s} bash_alias/g' index.html"
