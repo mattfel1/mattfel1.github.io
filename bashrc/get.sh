d5838a7ca0c784c210a23dab1a52e5c4e73183a5
Matthew <mattfel@stanford.edu>
2016-12-20 16:10:09 -0800

    auto update

--- bashrc/get.sh
+++ bashrc/get.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-servers=("max-2" "tucson" "portland" "london")
+servers=("max-2.stanford.edu" "tucson.stanford.edu" "portland.stanford.edu" "london.stanford.edu" "mfeldman@portal.maxeler.com")
 for s in ${servers[@]}; do
-  scp -o ConnectTimeout=1 ${s}.stanford.edu:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.sh
-  scp -o ConnectTimeout=1 ${s}.stanford.edu:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.sh
+  echo "scp -o ConnectTimeout=1 ${s}:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.sh"
+  echo "scp -o ConnectTimeout=1 ${s}:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.sh"
 done
 
+cd ~/mattfel1.github.io/bashrc
+git add -A
+git commit -m "auto update"
+git push
 
