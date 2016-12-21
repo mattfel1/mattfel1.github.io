a84be6962b53cda7e63b89c9a4738b2bbbd6936b
Matthew <mattfel@stanford.edu>
2016-12-20 16:23:26 -0800

    auto update

--- bashrc/get.sh
+++ bashrc/get.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-servers=("max-2.stanford.edu" "tucson.stanford.edu" "portland.stanford.edu" "london.stanford.edu" "-p=3033=mfeldman@portal.maxeler.com")
-for ss in ${servers[@]}; do
-  s=(`echo $ss | sed "i/=/ /g"`)
-  echo "scp -o ConnectTimeout=1 ${s}:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.sh"
-  echo "scp -o ConnectTimeout=1 ${s}:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.sh"
+cd ~/mattfel1.github.io/bashrc
+servers=("max-2" "tucson" "portland" "london" "maxeler")
+for s in ${servers[@]}; do
+  if [[ $s = "maxeler" ]]; then
+    cmd="-P 3033 mfeldman@portal.maxeler.com"
+  else
+    cmd="${s}.stanford.edu"
+  fi
+  scp -o ConnectTimeout=1 ${cmd}:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.sh
+  if [[ $? = 0 ]]; then # success
+    sed 's/rgb(.*)">$s bashrc/rgb(0,0,255)">$s bashrc/g' index.html
+  else # fail
+    sed 's/rgb(.*)">$s bashrc/rgb(255,0,0)">$s bashrc/g' index.html
+  fi
+  scp -o ConnectTimeout=1 ${cmd}:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.sh
+  if [[ $? = 0 ]]; then # success
+    sed 's/rgb(.*)">$s bashrc/rgb(0,0,255)">$s bashrc/g' index.html
+  else # fail
+    sed 's/rgb(.*)">$s bashrc/rgb(255,0,0)">$s bashrc/g' index.html
+  fi
 done
 
 cd ~/mattfel1.github.io/bashrc
