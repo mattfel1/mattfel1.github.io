f415731d805d3dafc44aa32018e6fdaab6cf2f69
Matthew <mattfel@stanford.edu>
2016-12-20 16:48:21 -0800

    auto update

--- bashrc/get.sh
+++ bashrc/get.sh
@@ -10,24 +10,24 @@ for s in ${servers[@]}; do
   fi
   scp -o ConnectTimeout=1 ${cmd}:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.sh
   if [[ $? = 0 ]]; then # success
-    st="sed -i 's/rgb(.*)\">${s} bashrc/rgb(0,0,255)\">${s} bashrc/g' index.html"
+    st="sed -i 's/rgb(.*)\">.*${s} bashrc/rgb(0,0,255)\">✓ ${s} bashrc/g' index.html"
     eval $st
   else # fail
-    st="sed -i 's/rgb(.*)\">${s} bashrc/rgb(255,0,0)\">${s} bashrc/g' index.html"
+    st="sed -i 's/rgb(.*)\">.*${s} bashrc/rgb(255,0,0)\">× ${s} bashrc/g' index.html"
     eval $st
   fi
   scp -o ConnectTimeout=1 ${cmd}:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.sh
   if [[ $? = 0 ]]; then # success
-    st="sed -i 's/rgb(.*)\">${s} bash_alias/rgb(0,0,255)\">${s} bash_alias/g' index.html"
+    st="sed -i 's/rgb(.*)\">.*${s} bash_alias/rgb(0,0,255)\">✓ ${s} bash_alias/g' index.html"
     eval $st
   else # fail
-    st="sed -i 's/rgb(.*)\">${s} bash_alias/rgb(255,0,0)\">${s} bash_alias/g' index.html"
+    st="sed -i 's/rgb(.*)\">.*${s} bash_alias/rgb(255,0,0)\">× ${s} bash_alias/g' index.html"
     eval $st
   fi
 done
 
-#cd ~/mattfel1.github.io/bashrc
-#git add -A
-#git commit -m "auto update"
-#git push
+cd ~/mattfel1.github.io/bashrc
+git add -A
+git commit -m "auto update"
+git push
 
