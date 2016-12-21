bd7fc2d8d63d175afe8c66ac7ea600a09f174c50
Matthew <mattfel@stanford.edu>
2016-12-20 16:11:20 -0800

    auto update

--- bashrc/get.sh
+++ bashrc/get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-servers=("max-2.stanford.edu" "tucson.stanford.edu" "portland.stanford.edu" "london.stanford.edu" "mfeldman@portal.maxeler.com")
+servers=("max-2.stanford.edu" "tucson.stanford.edu" "portland.stanford.edu" "london.stanford.edu" "-p 3033 mfeldman@portal.maxeler.com")
 for s in ${servers[@]}; do
   echo "scp -o ConnectTimeout=1 ${s}:~/.bashrc ~/mattfel1.github.io/bashrc/${s}_bashrc.sh"
   echo "scp -o ConnectTimeout=1 ${s}:~/.bash_aliases ~/mattfel1.github.io/bashrc/${s}_bash_aliases.sh"
